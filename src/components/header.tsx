976af0ae43818e4e80a46a9fa16e646b715204d6
Femar 01 <femar01@femar.local>
2023-02-16 17:37:30 -0300

    header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,22 @@
 "use client";
 import { ChevronRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex h-20 items-center  border-b border-gray-200">
+    <div className="flex h-16 items-center  border-b border-gray-200">
       <div className="mx-auto flex h-full w-full max-w-[1200px] items-center justify-between">
-        <h1 className="text-2xl font-bold tracking-tighter">St. Patrick</h1>
+        <Link href="/" className="text-2xl font-bold tracking-tighter">
+          St. Patrick
+        </Link>
         <nav>
           <div className="flex items-center gap-4">
-            <button className="flex items-center gap-1 rounded-full border py-2 px-4 text-sm font-light">
-              Hacete cliente
-              <ChevronRightIcon />
-            </button>
+            <Link href="/personas">
+              <button className="flex items-center gap-1 rounded-full border py-2 px-4 text-sm font-light">
+                Hacete cliente
+                <ChevronRightIcon />
+              </button>
+            </Link>
             <button className="flex items-center gap-1 rounded-full bg-black py-2 px-4 text-sm font-light text-white">
               Online Banking
               <ChevronRightIcon />
