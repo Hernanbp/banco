5497275fd98bf6b5204ab7a914b30c48b0c276f0
Hernan <her.cw@hotmail.com>
2023-02-16 20:20:07 -0300

    about section

--- src/app/personas/page.tsx
+++ src/app/personas/page.tsx
@@ -1,39 +1,44 @@
 "use client";
+
 import React from "react";
-import { CheckIcon, DashIcon } from "@radix-ui/react-icons";
+import { CheckIcon, DashIcon, InfoCircledIcon } from "@radix-ui/react-icons";
 
 const page = () => {
   return (
     <>
-      <div className="mx-auto flex h-screen max-w-[1200px] flex-col justify-center">
-        <div className="max-w-[550px] lg:mb-20">
-          <p className="font-semibold  text-lime-500">Dejá de pensarlo</p>
-          <h2 className="text-[2.5rem] font-bold  leading-[120%]">
-            Hacete cliente de St. Patrick ¡Te estamos esperando!
-          </h2>
-          <ul className="mt-4 flex flex-col gap-2 text-lg">
-            <li className="flex items-center">
-              <CheckIcon className="mr-2 scale-150 text-lime-500" />
-              Aprovechá todos los productos y beneficios que tenemos para vos
-            </li>
-            <li className="flex items-center">
-              <CheckIcon className="mr-2 scale-150 text-lime-500" />
-              Acumulá Puntos St. Patrick en todas tus compras
-            </li>
-            <li className="flex items-center">
-              <CheckIcon className="mr-2 scale-150 text-lime-500" />
-              Disfrutá descuentos en Go de St. Patrick, pagá con MODO y más
-            </li>
-          </ul>
-          <button className="mt-10 rounded-full bg-lime-500 py-4 px-8 font-semibold">
-            Quiero ser cliente
-          </button>
-          <p className="mt-6 font-semibold">Bienvenido a la nueva bancalidad</p>
+      <div className="bg-gray-50">
+        <div className="mx-auto flex h-[700px] max-w-[1200px] flex-col justify-center">
+          <div className="max-w-[550px] lg:mt-24">
+            <p className="font-semibold  text-lime-500">Dejá de pensarlo</p>
+            <h2 className="text-[2.5rem] font-bold  leading-[120%]">
+              Hacete cliente de St. Patrick ¡Te estamos esperando!
+            </h2>
+            <ul className="mt-4 flex flex-col gap-2 text-lg">
+              <li className="flex items-center">
+                <CheckIcon className="mr-2 scale-150 text-lime-500" />
+                Aprovechá todos los productos y beneficios que tenemos para vos
+              </li>
+              <li className="flex items-center">
+                <CheckIcon className="mr-2 scale-150 text-lime-500" />
+                Acumulá Puntos St. Patrick en todas tus compras
+              </li>
+              <li className="flex items-center">
+                <CheckIcon className="mr-2 scale-150 text-lime-500" />
+                Disfrutá descuentos en Go de St. Patrick, pagá con MODO y más
+              </li>
+            </ul>
+            <button className="mt-10 rounded-full bg-lime-500 py-4 px-8 font-semibold">
+              Quiero ser cliente
+            </button>
+            <p className="mt-6 font-semibold">
+              Bienvenido a la nueva bancalidad
+            </p>
+          </div>
         </div>
       </div>
       <div className="bg-lime-500">
         <div className="mx-auto flex h-80 max-w-[1200px] flex-col justify-center py-10">
-          <h2 className="text-2xl font-semibold">
+          <h2 className="text-[1.63rem] font-semibold">
             Viví una verdadera experiencia en banca
           </h2>
           <h3 className="my-4 mt-2 text-xl">
@@ -56,6 +61,64 @@ const page = () => {
               tranquilidad de tu hogar, contando con atención personalizada
             </li>
           </ul>
+        </div>
+      </div>
+      <div className="bg-gray-50">
+        <div className="mx-auto flex h-[40rem] max-w-[1200px] items-center justify-between">
+          <div className="flex h-[500px] w-[360px] flex-col items-center justify-center rounded-md border bg-white p-8 text-center drop-shadow-sm">
+            <h2 className="text-3xl font-bold">Sacá tu tarjeta de crédito</h2>
+            <h3 className="mt-2 text-base font-semibold">
+              Sacá tu tarjeta de crédito gratis por 1 año
+            </h3>
+            <p className="mt-5">
+              Pedila en 5 minutos, accedé a descuentos en viajes, preventas,
+              indumentaria y mucho más.
+            </p>
+            <button className="mt-10 rounded-full border bg-lime-500 py-3 px-6 font-medium">
+              Solicitá tu tarjeta
+            </button>
+            <p className="mt-10 flex items-center gap-2 font-semibold">
+              <InfoCircledIcon className="scale-125" />
+              Más información
+            </p>
+          </div>
+          <div className="flex h-[500px] w-[360px] flex-col items-center justify-center rounded-md border bg-white p-6 text-center drop-shadow-sm">
+            <h2 className="text-3xl font-bold">
+              Cobrá tu sueldo en St. Patrick
+            </h2>
+            <h3 className="mt-2 text-base font-semibold">
+              No te pierdas los beneficios exclusivos de cobrar tu sueldo en St.
+              Patrick
+            </h3>
+            <p className="mt-5">
+              Disfrutá de tu cuenta 100% bonificada, tasas exclusivas en
+              préstamos personales y descuentos en seguros.
+            </p>
+            <button className="mt-10 rounded-full border bg-lime-500 py-3 px-6 font-medium">
+              Solicitá tu tarjeta
+            </button>
+            <p className="mt-10 flex items-center gap-2 font-semibold">
+              <InfoCircledIcon className="scale-125" />
+              Mas informacion
+            </p>
+          </div>
+          <div className="flex h-[500px] w-[360px] flex-col items-center justify-center rounded-md border bg-white p-6 text-center drop-shadow-sm">
+            <h2 className="text-3xl font-bold">Acreditá tus ventas</h2>
+            <h3 className="mt-2 text-base font-semibold">
+              Te apoyamos con tu comercio
+            </h3>
+            <p className="mt-5">
+              Acreditá tus ventas con tarjetas de crédito y débito en una cuenta
+              corriente en BBVA y tené beneficios para vos y tus clientes.
+            </p>
+            <button className="mt-10 rounded-full border bg-lime-500 py-3 px-6 font-medium">
+              Adherite
+            </button>
+            <p className="mt-10 flex items-center gap-2 font-semibold">
+              <InfoCircledIcon className="scale-125" />
+              Más información
+            </p>
+          </div>
         </div>
       </div>
     </>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,25 +4,49 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex h-16 items-center border-b  border-gray-200 bg-white">
-      <div className="mx-auto flex h-full w-full max-w-[1200px] items-center justify-between">
+    <div className="fixed top-0 left-0 flex h-16 w-full flex-col items-center border-b border-gray-200 bg-white   ">
+      <div className="mx-auto flex h-full w-full max-w-[1200px] items-center justify-between py-6">
         <Link href="/" className="text-2xl font-bold tracking-tighter">
           St. Patrick
         </Link>
         <nav>
           <div className="flex items-center gap-4">
             <Link href="/personas">
-              <button className="flex items-center gap-1 rounded-full border py-2 px-4 text-sm font-light">
+              <button className="flex items-center gap-1 rounded-full border py-2 px-4 text-sm transition-all duration-200 ease-linear hover:border-black hover:text-black ">
                 Hacete cliente
                 <ChevronRightIcon />
               </button>
             </Link>
-            <button className="flex items-center gap-1 rounded-full bg-black py-2 px-4 text-sm font-light text-white">
+            <button className="flex items-center gap-1 overflow-hidden rounded-full bg-black py-2 px-4 text-sm  text-white  transition-all duration-200 ease-linear hover:bg-lime-500 hover:text-black">
               Online Banking
               <ChevronRightIcon />
             </button>
           </div>
         </nav>
+      </div>
+      <div className="flex h-full w-full items-center border-b border-t border-gray-200 bg-white py-4">
+        <div className="mx-auto w-full max-w-[1200px]">
+          <ul className=" flex gap-6 text-sm">
+            <li className="cursor-pointer font-semibold hover:text-lime-500">
+              Centro de ayuda
+            </li>
+            <li className="cursor-pointer font-semibold hover:text-lime-500">
+              Turnos Online
+            </li>
+            <li className="cursor-pointer font-semibold hover:text-lime-500">
+              Beneficios
+            </li>
+            <li className="cursor-pointer font-semibold hover:text-lime-500">
+              Tarjetas y Cuentas
+            </li>
+            <li className="cursor-pointer hover:text-lime-500">
+              Cuenta Sueldo
+            </li>
+            <li className="cursor-pointer hover:text-lime-500">Préstamos</li>
+            <li className="cursor-pointer hover:text-lime-500">Seguros</li>
+            <li className="cursor-pointer hover:text-lime-500">Superfondos</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
